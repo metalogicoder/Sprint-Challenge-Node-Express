1a445c51fea9530fc2f5e9f7335039f384ad6230
Devon Bacon <devon.bacon@gmail.com>
2018-05-04 08:14:57 -0700

    Added get request for data with specified id in server.js

--- server.js
+++ server.js
@@ -18,7 +18,21 @@ server.get('/api/:target', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        error: "The information could not be retrieved."
+        error: 'The information could not be retrieved.'
+      });
+    });
+});
+
+// get data from specific element in database using id
+server.get('/api/:target/:id', (req, res) => {
+  db[req.params.target]
+    .get(req.params.id)
+    .then(targetElem => {
+      res.json(targetElem);
+    })
+    .catch(err => {
+      res.status(404).json({
+        error: 'Data with the specified id does not exist.'
       });
     });
 });
