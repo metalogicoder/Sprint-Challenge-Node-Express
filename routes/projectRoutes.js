bf12225c3339dca07445401cb5c49b0a304c0638
Devon Bacon <devon.bacon@gmail.com>
2018-05-04 09:18:48 -0700

    Added functionality to projectRoutes to retrieve actions

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,4 +4,15 @@ const db = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+router.get('/:id/actions', (req, res) => {
+  db
+    .getProjectActions(req.params.id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(err => {
+      error: 'The information could not be retrieved.'
+    })
+})
+
 module.exports = router;
