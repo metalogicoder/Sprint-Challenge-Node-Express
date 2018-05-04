a143611c010559097136d529abc0eff7fcf37dc7
Devon Bacon <devon.bacon@gmail.com>
2018-05-04 08:06:48 -0700

    Added get request for all target data of the database in server.js

--- server.js
+++ server.js
@@ -9,4 +9,18 @@ const db = {
   projects: require('./data/helpers/projectModel.js')
 }
 
+// get data from target in database
+server.get('/api/:target', (req, res) => {
+  db[req.params.target]
+    .get()
+    .then(targetData => {
+      res.json(targetData);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The information could not be retrieved."
+      });
+    });
+});
+
 server.listen(5000, console.log('\n== API Running on port 5000 ==\n'));
