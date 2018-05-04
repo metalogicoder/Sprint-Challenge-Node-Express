ef8dc8fb2aa081fffd80c616969e8958eb537a12
Devon Bacon <devon.bacon@gmail.com>
2018-05-04 08:00:52 -0700

    Pulled actions and projects into server.js while enabling JSON format

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+const db = {
+  actions: require('./data/helpers/actionModel'),
+  projects: require('./data/helpers/projectModel.js')
+}
+
 server.listen(5000, console.log('\n== API Running on port 5000 ==\n'));
